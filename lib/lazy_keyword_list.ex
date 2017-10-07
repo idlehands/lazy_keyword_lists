b5d6184148531a6d74af0b360d1fe7aac6cdc4c7
Jeffrey Matthias <jmatthias@weedmaps.com>
2017-10-06 22:38:59 -0600

    Start with basic functionality. Add questionable default value. Does not support string interpolation.

--- lib/lazy_keyword_list.ex
+++ lib/lazy_keyword_list.ex
@@ -1,18 +1,46 @@
 defmodule LazyKeywordList do
-  @moduledoc """
-  Documentation for LazyKeywordList.
-  """
+  defmacro sigil_k({:<<>>, _line, [term_string]}, _modifiers) do
+    {terms, default} = terms_and_default(term_string)
 
-  @doc """
-  Hello world.
+    validate_names(terms)
 
-  ## Examples
+    keyword_list(terms, default)
+  end
 
-      iex> LazyKeywordList.hello
-      :world
+  defp terms_and_default(term_string) do
+    terms = String.split(term_string)
+    case Enum.at(terms, -2) do
+      "|" -> split_terms_and_default(terms)
+      _   -> {terms, nil}
+    end
+  end
 
-  """
-  def hello do
-    :world
+  defp keyword_list(terms, nil) do
+    Enum.map(terms, fn(term) ->
+      atom_term = String.to_atom(term)
+      {atom_term, Macro.var(atom_term, nil)}
+    end)
+  end
+  defp keyword_list(terms, default) do
+    Enum.map(terms, fn(term) ->
+      {String.to_atom(term), default}
+    end)
+  end
+
+  defp split_terms_and_default(terms) do
+    {raw_default, terms} = List.pop_at(terms, -1)
+    {_, terms} = List.pop_at(terms, -1)
+    {default, []} = Code.eval_string(raw_default)
+    {terms, default}
+  end
+
+  defp validate_names(terms) do
+    Enum.each terms, fn(term) ->
+      unless term =~ ~r/^[a-z]{1}/ do
+        raise ArgumentError,
+          "#{term} invalid: names must start with a lower case letter"
+      end
+    end
   end
 end
+
--- test/lazy_keyword_list_test.exs
+++ test/lazy_keyword_list_test.exs
@@ -1,8 +1,26 @@
 defmodule LazyKeywordListTest do
   use ExUnit.Case
-  doctest LazyKeywordList
+  import LazyKeywordList
 
-  test "greets the world" do
-    assert LazyKeywordList.hello() == :world
+  describe "basic use:" do
+    test "returns simple keyword list with variables" do
+      bob = "value1"
+      jim = "value2"
+
+      assert ~k(bob jim) === [bob: bob, jim: jim]
+    end
+
+    test "accepts default value" do
+      assert ~k(bob jim | true) === [bob: true, jim: true]
+    end
+  end
+
+  describe "validations" do
+    test "raises on invalid key/variable name" do
+      assert_raise ArgumentError, ~r/1 invalid: names must start with a lower case letter/, fn ->
+        bad_call = quote do: ~k(1)
+        Code.eval_quoted(bad_call)
+      end
+    end
   end
 end
