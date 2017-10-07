07107d601a11c3779177d3eae87338cb93ab9a0e
Jeffrey Matthias <jmatthias@weedmaps.com>
2017-10-06 20:13:32 -0600

    Initial commit.

---
+++ lib/lazy_keyword_list.ex
@@ -0,0 +1,18 @@
+defmodule LazyKeywordList do
+  @moduledoc """
+  Documentation for LazyKeywordList.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LazyKeywordList.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lazy_keyword_list_test.exs
@@ -0,0 +1,8 @@
+defmodule LazyKeywordListTest do
+  use ExUnit.Case
+  doctest LazyKeywordList
+
+  test "greets the world" do
+    assert LazyKeywordList.hello() == :world
+  end
+end
